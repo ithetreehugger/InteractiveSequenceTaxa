94a687a729cf850cc1c7a614eae2178a10d5f0b2
ithetreehugger <53713244+ithetreehugger@users.noreply.github.com>
2019-12-16 20:03:19 -0500

    Update main.py
    add documentation

--- metagenomics/main.py
+++ metagenomics/main.py
@@ -13,36 +13,58 @@ from bokeh.palettes import Set3
 from bokeh.events import Tap
 from bokeh.transform import stack
 
+# The expected columns from the sequence_taxa files 
 original_columns = ['##Sequence_ID', 'Hit_Coordinates', 'NCBI_Taxon_ID', 'Taxon_Rank', 'Taxon_Name', 'Probability_Mass',
                  'Markers_Hit']
+
+# The taxa ranks that will be display-able
 taxa_rank_order = ["species", "genus", "family", "order", "class", "phylum", "no rank"]
 
+# Reading in the lineage data as a dataframe
 lineage_df = pd.read_csv("lineages-2019-12-08-abridged2.csv")
 
+# global variable for storing filters in the form of a lambda and a value, which are later applied to the active input
 source_filter = {}
 
+# generate the source columns for the bokeh data table
 columns: List[TableColumn] = [TableColumn(field=k, title=k) for k in original_columns]
+# Storage for the data related to all input files; starts empty
 inputs: Dict = {}
 
   
 def getActiveSource():
+    """
+    Getter function for the bokeh source object of the active input
+    """
     return inputs[active_input]['source']
 
   
 def getActiveDf():
+    """
+    Getter function for the pandas dataframe object of the active input
+    """
     return inputs[active_input]['df']
 
 
 def getActiveTable():
+    """
+    Getter function for the bokeh data table object of the active input
+    """
     return inputs[active_input]['table']
 
   
 def generateLineageDf(df: pd.DataFrame):
+    """
+    Filter global lineage dataframe for only the taxon ids in the given dataframe, df 
+    """
     ids = df['NCBI_Taxon_ID']
     return lineage_df[lineage_df.tax_id.isin(ids)]
 
   
 def initDataSets():
+    """
+    initialize a dataset for the first time. If the dataset has already been initialized, this will do nothing.
+    """
     global inputs
     for name, d in inputs.items():
         if "source" in d.keys():
@@ -55,10 +77,14 @@ def initDataSets():
         inputs[name]['table'].sizing_mode = "stretch_both"
 
 
+# global variable to store the currently selected input        
 active_input = None
 
 
 def activateInput(name: str) -> Tuple[DataTable, ColumnDataSource]:
+    """
+    activates the input given by name. name must exist in the global inputs dictionary
+    """
     global active_input
     if active_input is None:
         return False
@@ -68,17 +94,25 @@ def activateInput(name: str) -> Tuple[DataTable, ColumnDataSource]:
     return True
 
 
+# Create the probability Mass slider.
 prob_slider = Slider(start=0.00, end=1.00, step=.01, value=0.5, title="Probability Mass")
+# Create the taxa rank drop down menu
 taxa_menu = Dropdown(label="Taxon Rank", menu=taxa_rank_order, value=taxa_rank_order[-1])
+# create the file choose 
 file_chooser = FileInput(id="Load Sequence Taxa Files", accept=".txt,.csv,.tsv")
+# create the input selection drop down menu
 input_menu = Dropdown(label="Inputs", menu=list(inputs.keys()))
+# create the reset button
 reset_button: Button = Button(label="Reset Settings", button_type="warning")
 
+# specify that we want all the following items in a single row    
 top_row = row(prob_slider, taxa_menu, input_menu, reset_button, file_chooser)
 
 
+# create a widgetbox (currently empty) to eventually store the data table
 data_box = widgetbox(sizing_mode='stretch_both')
 
+# If there is already an input during startup, activate it (only used for testing, really)
 if (activateInput(active_input)):
     data_table = getActiveTable()
     children = data_box.children
@@ -88,7 +122,11 @@ if (activateInput(active_input)):
         children[0] = data_table
 
 def generateFreqDf(named_sources):
-
+    """
+    Generate a dataframe containing the frequency of each taxa member for the currently selected taxa.
+    This not only counts the number of members of each taxa rank, but also aggregates the number of sub-rank members
+    which fall within the current rank-member category.
+    """
     frames = []
     for name, data in inputs.items():
         first_time = len(data['lineages']) == 1
@@ -113,19 +151,35 @@ def generateFreqDf(named_sources):
     return freq_df
 
 
+# global variable to hold the stacked graph data sources
 stack_source = None
 
+# Create the drop down menu which holds the members of the current rank
+#   (or sub members, if we are filtering on a specifc member)
 drill_down = Dropdown(label="Drill Down", menu=[])
 
 def click_bar_stack(event):
+    """
+    Was hoping to allow drill-down based on clicking on the stacked graph; didn't get it working, 
+    using dropdown menu instead
+    """
     selected = stack_source.selected.indices
 
 
+# store the last selected drill-down menu item
 last_drill_down = ""
+# store the last selecgted probability mass value
 last_prob = ""
+# store the last selected taxa rank value
 last_rank = ""
+# store the current drill-down sub-member value
 live_drill_val = ""
+
+
 def needs_update():
+    """
+    Update the data; ONLY if it needs to be updated. If nothing has changed, don't update.
+    """
     update = False
     for last, cur in zip([last_prob, last_drill_down, last_rank], [prob_slider.value, live_drill_val, live_taxa_val]):
         if last == cur:
@@ -135,6 +189,14 @@ def needs_update():
     return update
 
 def updateStackedGraph():
+    """
+    Generate a new stacked graph and replace the old one. 
+    First, get the frequency dataframes for each inputs lineages
+    Then, normalize each lineage frequency to represent proportion out of 100% instead of count.
+    Then, create each stacked bar, with alternating color pallete
+    
+    Lastly, this is usually the last graphical update to occur, so update the "last_" values
+    """
     freq_df = generateFreqDf(inputs)
 
     # Normalize the frequency to be out of 100 percent
@@ -184,6 +246,9 @@ def updateStackedGraph():
 
 
 def normalize_df(df: pd.Series):
+    """
+    normalize the df so each row is a fract out of 1
+    """
     df_sum = df.sum()
     res: pd.Series = df / df_sum
     res = res.apply(lambda x: x if x > .0009 else 0.0)
@@ -191,6 +256,9 @@ def normalize_df(df: pd.Series):
 
 
 def updateAllSources(**kwargs):
+    """
+    Update all the sources. If a drill down menu was selected, filter on the dril down sub-member selected before updating
+    """
     # Drill down was selected; lower the rank by one
     global last_drill_down
     global live_taxa_val
@@ -213,8 +281,13 @@ def updateAllSources(**kwargs):
     for name, dct in inputs.items():
         update_source(dct)
 
+# global boolean flag to know if the input has changed or not
 input_changed = False
+
 def update_source(data: Dict=None) -> None:
+    """
+    update a source. This is done by applying any sources in the source filter for the given data
+    """
     cur_df: pd.DataFrame = data['df'] if data is not None else getActiveDf()
     src = data['source'] if data is not None else getActiveSource()
     mask = None
@@ -253,22 +326,33 @@ def update_source(data: Dict=None) -> None:
 
 
 def prob_slider_callback(attr, old, new):
-
+    """
+    function triggered when the probabiltiy mass slider is changed; 
+    updates the source filter, and triggers an update if needed
+    """
     mask = lambda df, x: df.Probability_Mass >= x
     source_filter['prob'] = (mask, new)
     updateIfNeeded()
 
+# global flag to block and update if we are setting multiple values programmatically.
 manual_change = False
 
 def updateIfNeeded():
+    """
+    Checks if an update is necessary before triggering an update;
+    """
     if needs_update() and not manual_change:
         updateAllSources()
         updateStackedGraph()
 
 
+# Currently active taxa value
 live_taxa_val = taxa_rank_order[-1]
 def taxa_menu_callback(attr, old, new):
-
+    """
+    function to trigger when the taxa rank drop down menu changes.
+    update if needed.
+    """
     global live_taxa_val
     live_taxa_val = new
     label = "Taxon Rank"
@@ -279,9 +363,12 @@ def taxa_menu_callback(attr, old, new):
     live_drill_val = None
     updateIfNeeded()
 
+# Initialize the probability mass value to .5
 live_prob = .5
 def input_menus_callback(attr, old, new):
-
+    """
+    function to trigger when the input selection is changed. Updates the data table
+    """
     if old == new:
         return
 
@@ -300,7 +387,11 @@ def input_menus_callback(attr, old, new):
 
     input_changed = False
 
+    
 def drill_down_callback(attr, old, new):
+    """
+    Function triggered when the drill-down menu is updated. updates if needed.
+    """
     global live_drill_val
     live_drill_val = new
 
@@ -311,6 +402,13 @@ def drill_down_callback(attr, old, new):
 
 
 def reset_settings():
+    """
+    Reset the values of all UI objects to default values;
+    probability slides get .5
+    taxa rank gets the highest taxa rank
+    active input set to first input
+    drill-down menu reset to none
+    """
     global manual_change
     global last_prob
     global last_drill_down
@@ -330,6 +428,10 @@ def reset_settings():
     updateIfNeeded()
 
 def add_input_file(attr, old, new):
+    """
+    function triggered when a new input file is uploaded.
+    decode the contents, read as a dataframe, add to the inputs, and initialize the data
+    """
     contents = base64.standard_b64decode(new)
     df = pd.read_csv(io.BytesIO(contents), sep='\t')
     df = df[df.Taxon_Rank.isin(taxa_rank_order)]
@@ -347,7 +449,7 @@ def add_input_file(attr, old, new):
     else:
         reset_settings()
 
-
+# Set the callback triggers for all the interactive UI elements
 prob_slider.on_change('value', prob_slider_callback)
 taxa_menu.on_change('value', taxa_menu_callback)
 file_chooser.on_change('value', add_input_file)
@@ -355,11 +457,16 @@ input_menu.on_change('value', input_menus_callback)
 drill_down.on_change('value', drill_down_callback)
 reset_button.on_click(reset_settings)
 
+# ensure the stacked graph fits the screen
 stacked_graph_row = column(sizing_mode="stretch_both")
 
+# Set up the entire web-page as a a single column, containing a top row, and a bottom row
 page = column(top_row, data_box, row(stacked_graph_row, drill_down))
 
 def start():
+    """
+    Start function first called by bokeh
+    """
     global manual_change
     # Turn off auto-update until we are down setting all, then call manually
     manual_change = True
@@ -373,6 +480,8 @@ def start():
     manual_change = False
     updateIfNeeded()
 
+# Set the web page size mode
 page.sizing_mode = "stretch_both"
 
+# add the page to the current bokeh document
 curdoc().add_root(page)
